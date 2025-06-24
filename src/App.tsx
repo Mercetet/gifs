e0fc2a46762b5664e914b72dd2e7de8d83279673
Mercedes Tetilla Le Donne <127346103+Mercetet@users.noreply.github.com>
2025-06-23 22:06:32 -0300

    gifsApp

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { NavBar } from './components/ui/NavBar/NavBar'
+import { GiftList } from './components/GiftList/GiftList'
+
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <GiftList />
+    </>
+  )
+}
+
+export default App
